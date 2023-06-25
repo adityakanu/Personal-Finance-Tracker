e8ea15495f094661e7ff84a497181dc64fdd4d7f
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-06-25 23:55:25 +0530

    removed unnecessary dependencies and fixed api leak issue

--- src/Auth/firebaseLogin.js
+++ src/Auth/firebaseLogin.js
@@ -1,7 +1,20 @@
+//import api keys for firebase
+
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MEASUREMENT_ID,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from "./firebasekeys.js";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
 import {
+  GithubAuthProvider,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   getAuth,
@@ -11,13 +24,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABGF1v0w43s84ptrzQJijen35DlpaRL5E",
-  authDomain: "finance-tracker-login.firebaseapp.com",
-  projectId: "finance-tracker-login",
-  storageBucket: "finance-tracker-login.appspot.com",
-  messagingSenderId: "568099662115",
-  appId: "1:568099662115:web:f78ed70dd961eaf985c496",
-  measurementId: "G-F9PBE5F6MK",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -46,9 +59,9 @@ submitData.addEventListener("click", (e) => {
 
 // Google Signup
 
-const provider = new GoogleAuthProvider(app);
+const Googleprovider = new GoogleAuthProvider(app);
 GoogleSignup.addEventListener("click", (e) => {
-  signInWithPopup(auth, provider)
+  signInWithPopup(auth, Googleprovider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
@@ -66,6 +79,32 @@ GoogleSignup.addEventListener("click", (e) => {
       const email = error.customData.email;
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+});
+
+// Github Auth
+const Gitprovider = new GithubAuthProvider();
+
+GithubSignup.addEventListener("click", (e) => {
+  signInWithPopup(auth, Gitprovider)
+    .then((result) => {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GithubAuthProvider.credentialFromError(error);
       // ...
     });
 });
